1f25a96d5ccd06a3bd7d267b3cc732f41f0fd105
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-06-29 21:52:15 +0900

    First commit

---
+++ L/L.h
@@ -0,0 +1,19 @@
+//
+//  L.h
+//  L
+//
+//  Created by Yoshiki Kudo on 2016/06/29.
+//  Copyright © 2016年 Yoshiki Kudo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for L.
+FOUNDATION_EXPORT double LVersionNumber;
+
+//! Project version string for L.
+FOUNDATION_EXPORT const unsigned char LVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <L/PublicHeader.h>
+
+
