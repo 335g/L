56fd2f6fc80375227c261149130cd8e832911cd1
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-07-08 20:38:52 +0900

    Add Getter/Setter

--- L/Getter.swift
+++ L/Getter.swift
@@ -5,3 +5,22 @@
 public protocol GetterType: SimpleOpticsType {
 	func get(from: Source) -> Target
 }
+
+// MARK: - Getter
+
+public struct Getter<S, A> {
+	private let _get: (S) -> A
+	
+	public init(get: (S) -> A) {
+		_get = get
+	}
+}
+
+extension Getter: GetterType {
+	public typealias Source = S
+	public typealias Target = A
+	
+	public func get(from: S) -> A {
+		return _get(from)
+	}
+}
--- L/Setter.swift
+++ L/Setter.swift
@@ -11,3 +11,45 @@ public extension SetterType {
 		return modify(x, as: { _ in y })
 	}
 }
+
+// MARK: - LSetter
+
+public struct LSetter<S, T, A, B> {
+	private let _modify: (S, (A) -> B) -> T
+	
+	public init(modify: (S, (A) -> B) -> T) {
+		_modify = modify
+	}
+}
+
+extension LSetter: SetterType {
+	public typealias Source = S
+	public typealias AltSource = T
+	public typealias Target = A
+	public typealias AltTarget = B
+	
+	public func modify(_ x: S, as f: (A) -> B) -> T {
+		return _modify(x, f)
+	}
+}
+
+// MARK: - Setter
+
+public struct Setter<S, A> {
+	private let _modify: (S, (A) -> A) -> S
+	
+	public init(modify: (S, (A) -> A) -> S) {
+		_modify = modify
+	}
+}
+
+extension Setter: SetterType {
+	public typealias Source = S
+	public typealias AltSource = S
+	public typealias Target = A
+	public typealias AltTarget = A
+	
+	public func modify(_ x: S, as f: (A) -> A) -> S {
+		return _modify(x, f)
+	}
+}
